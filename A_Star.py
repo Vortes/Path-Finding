346f26e9ea375e28f265f0d0a436fa83f87593d4
Vortes <zeyu.weng@gmail.com>
2019-12-30 11:33:04 -0500

    Fixed the issue of the window not closing

--- A_Star.py
+++ A_Star.py
@@ -1,14 +1,16 @@
 import pygame
 pygame.init()
+pygame.display.set_caption('A Star Pathfinding')
 
 SIZE = WIDTH, HEIGHT = 500, 500
 SCREEN = pygame.display.set_mode(SIZE)
+CLOCK = pygame.time.Clock()
 
 RUNNING = True
 
 while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            RUNNING = False
  
 pygame.quit()
