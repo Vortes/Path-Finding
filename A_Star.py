91e266f3205a92e6e6e0442429d529a23559fb36
Vortes <zeyu.weng@gmail.com>
2019-12-30 13:08:41 -0500

    Finished grid layout

--- A_Star.py
+++ A_Star.py
@@ -2,15 +2,41 @@ import pygame
 pygame.init()
 pygame.display.set_caption('A Star Pathfinding')
 
-SIZE = WIDTH, HEIGHT = 500, 500
-SCREEN = pygame.display.set_mode(SIZE)
+SCREEN = pygame.display.set_mode((255, 255))
 CLOCK = pygame.time.Clock()
 
-RUNNING = True
 
+WIDTH, HEIGHT = 20, 20
+MARGIN = 5
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+SCREEN.fill(BLACK)
+
+
+def create_board():
+    for column in range(0, 255, WIDTH + MARGIN):
+        for row in range(0, 255, HEIGHT + MARGIN):
+            pygame.draw.rect(SCREEN, WHITE, (column + MARGIN, row + MARGIN, WIDTH, HEIGHT))
+
+
+def create_grids():
+    grid = []
+    for row in range(10):
+        grid.append([])
+        for column in range(10):
+            grid[row].append(0)
+    return grid
+
+grid = create_grids()
+print(grid)
+
+RUNNING = True
 while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
+
+    create_board()
+    pygame.display.update()
  
 pygame.quit()
