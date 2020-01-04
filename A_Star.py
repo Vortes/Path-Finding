4623c4be21ab181c5e51b5d3f6c14f439d3a8d16
Vortes <zeyu.weng@gmail.com>
2020-01-04 07:23:14 -0500

    Saving Progress

--- A_Star.py
+++ A_Star.py
@@ -1,70 +1,40 @@
-openList = []
-closedList = []
+class Node:
+    """Makes the cell, based on Formula: f(n) = g(n) + h(n) for A* search"""
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.f = 0
+        self.g = 0
+        self.h = 0
 
 
-class Cell:
-    """Makes the cell, based Formula: f(n) = g(n) + h(n) for A* search"""
-    def __init__(self, f=0, g=0, h=0):
-        self.f = f
-        self.g = g
-        self.h = h
+def aStar(maze, start, end):
+    start_node = Node(None, start)
+    end_node = Node(None, start)
 
+    open_list = []
+    closed_list = []
 
-class Grid:
-    """Makes the game board
+    open_list.append((start_node))
 
-    Attributes:
-        make_grid -- A 2D array with rows and columns specified by user
-        get_grid -- Returns the 2D array with formatting
-    """
-    grid = []
-
-    def __init__(self, row, column):
-        self.row = row
-        self.column = column
-
-    def make_grid(self):
-        for i in range(self.row):
-            self.grid.append([])
-            for j in range(self.column):
-                self.grid[i].append(0)
-        return self.grid
-
-    def get_grid(self):
-        for i in range(self.row):
-            print()
-            for j in range(self.column):
-                print(self.grid[i][j], end=' ')
-
-    def get_row(self):
-        return self.row
-
-    def get_column(self):
-        return self.column
-
-
-def initCell(game_board, row, column):
-    for i in range(row):
-        for j in range(column):
-            if i == 0 and j == 0:
-                game_board[0][0] = 0
-            else:
-                game_board[i][j] = Cell()
-
-    return game_board
+    while open_list:
+        pass
 
 
 def main():
-    grid = Grid(5, 5)
-    game_board = grid.make_grid()
-    game_board = initCell(game_board, grid.get_row(), grid.get_column())
-    start = game_board[0][0]
-    end = game_board[-1][-1]
-
-    openList.append(start)
+    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
     # Start the A* Search Loop!
-    while openList:
 
 
 
